455914ac24bb3c1731fdb0a8845a4bea40a5e38b
Michael Willer <me@michaelwiller.dk>
2024-04-09 17:22:34 +0200

    Update bin/backup.sh

--- bin/executable_backup.sh
+++ bin/executable_backup.sh
@@ -8,7 +8,7 @@ dest_dir="$2"
 mkdir -p "$dest_dir"
 
 # Perform tar operation with current date as part of the name
-tar_filename="backup_$(date +%Y-%m-%d).tar.gz"
+tar_filename="backup_$(date +%Y-%m-%d:%H-%M).tar.gz"
 tar -czf "$dest_dir/$tar_filename" -C "$source_dir" .
 
 # Clean up old tar files, keeping only the latest 7
