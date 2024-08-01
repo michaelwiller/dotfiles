aa8129624d09327b90582e4115b66acc2ab0d354
Michael Willer <michael.willer@enterprisedb.com>
2024-08-01 12:14:41 +0200

    Add bin/autorun/source_utmctl_complete.sh

---
+++ bin/autorun/source_utmctl_complete.sh
@@ -0,0 +1,43 @@
+_utmctl() {
+	local cur prev opts fav_options
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	#
+	#  The basic options we'll complete.
+	#
+	fav_options="edit copy-file connect help list refresh-local-env remove set-port stop"
+	opts=$fav_options
+
+	#
+	#  Complete the arguments to some of the basic commands.
+	#
+	case "${prev}" in
+
+		edit|help|list)
+			opts=""
+			;;
+
+		copy-file|\
+		connect|\
+		init|\
+		refresh-local-env|\
+		remove|\
+		set-port|\
+		stop)
+			opts=$(utmctl _names ${curr})
+			;;
+
+		-f)
+			opts=$(ls)
+			;;
+			
+		*)
+			;;
+	esac
+
+	COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+	return 0
+}
+complete -F _utmctl utmctl
