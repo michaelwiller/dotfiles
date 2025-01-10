e2a2646053ce70238483f680f1971d6612201437
Michael Willer <michael.willer@enterprisedb.com>
2025-01-10 17:13:53 +0100

    Update bin/autorun/source_kubernetes_complete.sh

--- bin/autorun/source_kubernetes_complete.sh
+++ bin/autorun/source_kubernetes_complete.sh
@@ -7,4 +7,5 @@ if which kubectl >/dev/null 2>&1; then
 	complete -o default -F __start_kubectl k
 	alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
 	alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+	alias kbash='f() { kubectl run -it temp-shell --rm -i --tty --image ubuntu -- bash ; } ; f'
 fi
