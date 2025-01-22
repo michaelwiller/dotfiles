f1bec420620e9f2a8c1022a13b07291e09970936
Michael Willer <michael.willer@enterprisedb.com>
2025-01-22 13:03:49 +0100

    Remove bin/backup-obsidan-cabinet.sh

--- bin/executable_backup-obsidan-cabinet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$HOME/bin/backup.sh /Users/michael.willer/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Cabinet /Users/michael.willer/Library/Mobile\ Documents/com~apple~CloudDocs/Personal/90\ Obsidian\ Backups/Cabinet
