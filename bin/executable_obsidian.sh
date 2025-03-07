ed7a33ea042debd6bd8fabdae38a68e036a53306
Michael Willer <michael.willer@enterprisedb.com>
2025-03-07 17:47:40 +0100

    Update bin/obsidian.sh

--- bin/executable_obsidian.sh
+++ bin/executable_obsidian.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+
+# Not used
+handle_dual(){
+	IFS='
+'
+
+	for dualfile in $(ls *\ 2.md)
+	do
+    		original_file=$(echo $dualfile | sed 's/ 2.md/.md/')
+    		if ! diff $dualfile $original_file > /dev/null 2>&1
+    		then
+			mv $dualfile $original_file
+    		else
+			rm $dualfile
+    		fi
+	done
+}
+
 backup_vault(){
 	targetdir=~/icloudlinks/obsidian-backups/$1
 	sourcedir=~/icloudlinks/obsidian-vaults/$1
