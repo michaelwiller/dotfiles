40417130a5c9fcad9c4e252986f9d3c04d621145
Michael Willer <michael.willer@enterprisedb.com>
2025-04-11 11:29:36 +0200

    Update bin/aws.sh

--- bin/executable_aws.sh
+++ bin/executable_aws.sh
@@ -27,6 +27,13 @@ image: Get all attributes for a specific image (Use this to find f.ex. owner-id)
 		ex. IMAGE_ID = ami-02fd62706c23e828f
 		REGION (defaults to eu-west-1)
 
+owner:
+
+	aws.sh owner IMAGE_ID [REGION]
+
+		ex. IMAGE_ID = ami-02fd62706c23e828f
+		REGION (defaults to eu-west-1)
+
 search: Look for images owned by a specific owner-id
 
 	aws.sh search OWNER_ID [REGION [SEARCH_STRING]]
@@ -48,6 +55,9 @@ case $action in
 	image)
 		describe_image "$@"
 		;;
+	owner)
+		describe_image "$@" | jq '.Images[].OwnerId'
+		;; 
 	search)
 		table_of_amis "$@"
 		;;
