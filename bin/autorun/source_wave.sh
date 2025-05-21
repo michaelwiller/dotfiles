35560587dd18f5fb8192680efc75a666b3f0f433
Michael Willer <michael.willer@enterprisedb.com>
2025-05-21 18:44:55 +0200

    Update bin/autorun/source_wave.sh

--- bin/autorun/source_wave.sh
+++ bin/autorun/source_wave.sh
@@ -1,6 +1,7 @@
 if [ -d /Applications/Wave.app ]; then
     wsh_cmd="~/Library/Application\ Support/waveterm/bin/wsh"
-    alias v="$wsh_cmd edit"
+    alias v="$wsh_cmd view"
+    alias e="$wsh_cmd edit"
     #alias wae="$wsh_cmd edit"
     #alias wap="$wsh_cmd view"
     #alias waw="$wsh_cmd web open"
